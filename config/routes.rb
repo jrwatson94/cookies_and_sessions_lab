7f99a091dfb4b56749c302e453a47ede9f4eb46a
jrwatson94 <j.watsonreid@gmail.com>
2020-10-19 19:47:05 -0400

    lab finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'application/cart', to: 'application#cart'
-  root "products#index"
+  
+  get '/', to: "products#index"
   post "/", to: "products#add"
 end
